dd46e900fc7c3d9eb1ebfdab83ca184e4e48f67f
Vitthal Sandip Dhawale <dhawalevs@rknec.edu>
2026-04-19 20:18:32 +0530

    Time: 0 ms (100%), Space: 102.3 MB (47.28%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
@@ -4,20 +4,29 @@ public:
         int ans = 0;
         int n = nums1.size();
         int m = nums2.size();
-        for(int i=0;i<n;i++){
-            int num = nums1[i];
-            int l = i;
-            int r = m-1;
-            while(l<=r){
-                int mid = (l+r)/2;
-                if(nums2[mid]>=num){
-                    l=mid+1;
-                }
-                else{
-                    r=mid-1;
-                }
+        // for(int i=0;i<n;i++){
+        //     int num = nums1[i];
+        //     int l = i;
+        //     int r = m-1;
+        //     while(l<=r){
+        //         int mid = (l+r)/2;
+        //         if(nums2[mid]>=num){
+        //             l=mid+1;
+        //         }
+        //         else{
+        //             r=mid-1;
+        //         }
+        //     }
+        //     if(r>=0 && nums2[r]>=num) ans = max(ans,(r-i));
+        // }
+        int i = 0;
+        int j = 0;
+        while(i<n && j<m){
+            if(nums2[j]>=nums1[i]){
+                ans = max(ans,j-i);
+                j++;
             }
-            if(r>=0 && nums2[r]>=num) ans = max(ans,(r-i));
+            else i++;
         }
         return ans;
     }
