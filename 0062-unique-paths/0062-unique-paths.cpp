e40d31eedf43245764b8691043cf417464711f66
Vitthal Sandip Dhawale <dhawalevs@rknec.edu>
2026-04-20 23:39:39 +0530

    Time: 0 ms (100%), Space: 9.4 MB (50.22%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -13,7 +13,7 @@ public:
     //     return path(m,n,dp);
     // }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1,vector<int>(n+1));
+        vector<vector<int>> dp(m,vector<int>(n));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(i==0 || j==0) dp[i][j]=1;
