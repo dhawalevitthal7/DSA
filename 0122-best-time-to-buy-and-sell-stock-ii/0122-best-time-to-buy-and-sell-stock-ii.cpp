c08e8eb4fda8f587bdfe4157b6ef522c8f3d7068
Vitthal Sandip Dhawale <dhawalevs@rknec.edu>
2026-07-20 17:57:52 +0530

    Time: 0 ms (100%), Space: 19.9 MB (97.74%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            if(i+1<n){
+                if(prices[i]<prices[i+1]){
+                    ans+=prices[i+1] - prices[i];
+                } 
+            }
+        }
+        return ans;
+    }
+};
