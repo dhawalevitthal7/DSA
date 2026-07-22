038a8ad8a781890fc657ebb14df1b77fcd0def0e
Vitthal Sandip Dhawale <dhawalevs@rknec.edu>
2026-07-22 15:23:06 +0530

    Time: 449 ms (65.82%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,10 +1,13 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-    SET N = N-1;
   RETURN (
-      # Write your MySQL query statement below.
-    SELECT DISTINCT salary FROM Employee
-    ORDER BY salary DESC 
-    LIMIT 1 OFFSET N
+    
+    with cte as(  # Write your MySQL query statement below.
+    SELECT DISTINCT  salary, dense_rank() over(order by salary desc) as rn FROM Employee
+    )
+
+    select salary as getNthHighestSalary from cte
+    where rn = N
+
   );
 END
