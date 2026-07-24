a9b7615e62741f2f9b5e80454f946a9ad7b585d4
Vitthal Sandip Dhawale <dhawalevs@rknec.edu>
2026-07-24 23:07:05 +0530

    Time: 0 ms (100%), Space: 8.2 MB (11.24%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,7 +4,7 @@ public:
         if(n==1) return 1;
         else if(n==0) return 0;
         if(dp[n]!=-1) return dp[n];
-        return func(n-1,dp)+func(n-2,dp);
+        return dp[n] = func(n-1,dp)+func(n-2,dp);
     }
     int fib(int n) {
         vector<int> dp(n+1,-1);
