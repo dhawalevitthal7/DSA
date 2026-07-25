e688389e6e1ed5bbdc76b17505b5ae2374d2259e
Vitthal Sandip Dhawale <dhawalevs@rknec.edu>
2026-07-25 22:39:42 +0530

    Time: 0 ms (100%), Space: 13.5 MB (26.51%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -12,7 +12,6 @@ public:
     }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        int m = triangle[0].size();
         vector<vector<int>> dp(n,vector<int>(n,0));
         for(int i=0;i<n;i++) dp[n-1][i] = triangle[n-1][i];
         return func(triangle,dp,n);
