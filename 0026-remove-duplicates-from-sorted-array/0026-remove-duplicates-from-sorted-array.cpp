d570cb68eea8a6b74fab6dbe0087735ff7c28dc5
Vitthal Sandip Dhawale <dhawalevs@rknec.edu>
2026-08-13 08:38:42 +0530

    Time: 0 ms (100%), Space: 22.7 MB (53.45%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        vector<int> v;
-        int temp = 0;
-        for(int i=0;i<nums.size();i++){
-            if(i==0){
-                temp = nums[i];
-                v.push_back(nums[i]);
-            }
-            else{
-                if(nums[i]==temp) continue;
-                else{
-                    temp = nums[i];
-                    v.push_back(nums[i]);
-                }
-            }
+    //     vector<int> v;
+    //     int temp = 0;
+    //     for(int i=0;i<nums.size();i++){
+    //         if(i==0){
+    //             temp = nums[i];
+    //             v.push_back(nums[i]);
+    //         }
+    //         else{
+    //             if(nums[i]==temp) continue;
+    //             else{
+    //                 temp = nums[i];
+    //                 v.push_back(nums[i]);
+    //             }
+    //         }
+    //     }
+    //     int k=0;
+    //    for(auto it : v){
+    //     nums[k++] = it;
+    //    }
+    //    return v.size();
+    int n = nums.size();
+    int i=-1,j=0;
+    while(j<n){
+        if(i==-1){
+            i++;
+            nums[i] = nums[j];
         }
-        int k=0;
-       for(auto it : v){
-        nums[k++] = it;
-       }
-       return v.size();
+        else if(nums[i]!=nums[j]){
+            i++;
+            nums[i] = nums[j];
+        }
+        j++;
+    }
+    return i+1;
     }
 };
